553321885d8f7bf0f126e82d15b7daf744281669
Ryan Horn <rj.horn@gmail.com>
2023-02-07 09:56:17 -0700

    ryry progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+    def record_invalid(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
